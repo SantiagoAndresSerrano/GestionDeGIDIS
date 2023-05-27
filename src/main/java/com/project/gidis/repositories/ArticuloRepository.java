2b2ec26fc3c765de199e2d2285516519a2d87ac2
Alejandro Urbina <alejandroarturousua@ufps.edu.co>
2023-05-27 16:43:37 -0500

    capa de Repositorios cargada

--- src/main/java/com/project/gidis/repositories/ArticuloRepository.java
+++ src/main/java/com/project/gidis/repositories/ArticuloRepository.java
@@ -1,9 +1,9 @@
 package com.project.gidis.repositories;
 
-import com.project.gidis.entities.Usuario;
+import com.project.gidis.entities.Articulo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
 }
--- src/main/java/com/project/gidis/repositories/ProyectoInvestigacionRepository.java
+++ src/main/java/com/project/gidis/repositories/ProyectoInvestigacionRepository.java
@@ -1,9 +1,9 @@
 package com.project.gidis.repositories;
 
-import com.project.gidis.entities.Usuario;
+import com.project.gidis.entities.ProyectoInvestigacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface ProyectoInvestigacionRepository extends JpaRepository<ProyectoInvestigacion, Long> {
 }

4ed9df85b531041b056b62791bd16c2c200e2f65
Alejandro Urbina <alejandroarturousua@ufps.edu.co>
2023-05-31 22:16:00 -0500

    Rework

--- src/main/java/com/project/gidis/dto/ProyectoInvestigacionRequestDto.java
+++ src/main/java/com/project/gidis/dto/ProyectoInvestigacionRequestDto.java
@@ -10,8 +10,16 @@ import lombok.*;
 public class ProyectoInvestigacionRequestDto {
 
     private String titulo;
+    private String tipoProyecto;
+    private String mesInicio;
     private String mesFin;
+    private String anioInicio;
     private String anioFin;
+    private boolean financiado;
     private String fuenteFinanciacion;
+    private String ambito;
+    private String resumen;
     private String instituciones;
+
+
 }
